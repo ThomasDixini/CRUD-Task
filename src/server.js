5a957cb71018b6b6efa5eab9aea5a401dbd33423
Thomás Dixini <thomasdixini@gmail.com>
2023-08-29 22:59:31 -0300

    :sparkles: feat: Mysql
    Criando conexão e primeiro usuário teste no DB MySql

--- src/server.js
+++ src/server.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import mysql from 'mysql'
 
 const app = express();
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'test',
+    password: '147258369',
+    database: 'my_db'
+})
+
+connection.connect();
+
+app.post('/', (req, res) => {
+    connection.query({
+        sql: `INSERT INTO task(id, name) VALUES(1,'Thomás')`
+    })
+
+    return res.end("Congratulations");
+})
 
 app.listen(3333, () => {
     return console.log("Hello world!")
